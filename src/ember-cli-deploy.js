55efe825e5952d20c6b519073c128ece7d92ee88
Chris LoPresto <chris@chrislopresto.com>
2015-11-10 00:00:00 -0500

    Write plugin in JavaScript

---
+++ src/ember-cli-deploy.js
@@ -0,0 +1,63 @@
+/* globals require, process, module, __dirname */
+// Description
+//   A Hubot script for Ember CLI Deploy
+//
+// Configuration:
+//   HUBOT_EMBER_CLI_DEPLOY_GITHUB_TOKEN: required
+//
+// Commands:
+//   hubot ember deploy <app> <options> - deploys an ember app with ember-cli-deploy
+//   continuous deployment - chimes in when the conversation gets rad
+//
+// Notes:
+//   <optional notes required for the script>
+//
+// Author:
+//   Chris LoPresto <chris@chrislopresto.com>
+
+var nodegit = require('nodegit');
+var fs = require('fs-extra');
+var path = require('path');
+var GITHUB_TOKEN = process.env.HUBOT_EMBER_CLI_DEPLOY_GITHUB_TOKEN;
+var scriptRootDirName = 'hubot-ember-cli-deploy';
+
+module.exports = function(robot) {
+  robot.respond(/ember deploy\s+(.+)/, function(res) {
+    var deployArgs = res.match[1];
+    var appName = deployArgs.split(' ')[0];
+    var localPath = path.join(__dirname, '..', scriptRootDirName, appName);
+    fs.ensureDirSync(localPath);
+    // var repoName = 'chrislopresto/bateman-ember';
+    // var repoName = 'yappbox/account';
+    var repoName = deployArgs;
+    var cloneURL = 'https://github.com/' + repoName + '.git';
+
+    console.log('Cloning: ', cloneURL);
+
+    var cloneOptions = {
+      remoteCallbacks: {
+        certificateCheck: function() {
+          return 1;
+        }
+      },
+      credentials: function() {
+        return nodegit.Cred.userpassPlaintextNew(GITHUB_TOKEN, '');
+      }
+    };
+
+    var cloneRepository = nodegit.Clone(cloneURL, localPath, cloneOptions);
+
+    var errorAndAttemptOpen = function() {
+      return nodegit.Repository.open(localPath);
+    };
+
+    cloneRepository.catch(errorAndAttemptOpen).then(function(repository) {
+      console.log('Is the repository bare? %s', Boolean(repository.isBare()));
+    });
+    return res.send('ok');
+  });
+
+  robot.hear(/continuous deployment/, function(res) {
+    return res.send('rad');
+  });
+};
--- test/ember-cli-deploy-test.coffee
+++ test/ember-cli-deploy-test.coffee
@@ -13,7 +13,7 @@ describe 'ember-cli-deploy', ->
     require('../src/ember-cli-deploy')(@robot)
 
   it 'registers a respond listener', ->
-    expect(@robot.respond).to.have.been.calledWith(/ember deploy/)
+    expect(@robot.respond).to.have.been.calledWith(/ember deploy\s+(.+)/)
 
   it 'registers a hear listener', ->
     expect(@robot.hear).to.have.been.calledWith(/continuous deployment/)
